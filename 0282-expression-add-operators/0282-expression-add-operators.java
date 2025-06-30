ec33c63b77d593498f47b839f70f55812a9a66fc
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-07-01 00:50:54 +0530

    Time: 124 ms (9.76%), Space: 45.8 MB (14.81%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,29 +1,35 @@
-public class Solution {
+class Solution {
     public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
+       List<String> response=new ArrayList<>();
+       recursion(0,num,target,"",0,0,response);
+       return response;
+
     }
-    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
-        if(pos == num.length()){
-            if(target == eval)
-                rst.add(path);
-            return;
-        }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
-            }
-            else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
-                
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
-                
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
-            }
-        }
+
+   private void recursion(int index,String num,int target,String exp,long prev,long res,List<String> response){
+    if(index>=num.length() ){
+     if(target==res){
+      response.add(exp);
+     }
+      return;
     }
+    
+    for(int j=index;j<num.length();j++){
+        if(j>index && num.charAt(index)=='0') break;
+        long number=Long.parseLong(num.substring(index,j+1));
+        if(index==0){
+            recursion(j+1,num,target,num.substring(index,j+1),number,number,response);
+        }
+        else{
+            recursion(j+1,num,target,exp+"+"+num.substring(index,j+1),number,res+number,response);
+                        recursion(j+1,num,target,exp+"-"+num.substring(index,j+1),-number,res-number,response);
+                        recursion(j+1,num,target,exp+"*"+num.substring(index,j+1),prev*number,res-prev+(prev*number),response);
+            
+        }
+
+    }
+    
+     
+   }
+
 }

83d22f718b5d1d5e7c228e7f53ad661d9a7e5276
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-07-02 20:38:34 +0530

    Time: 1 ms (100%), Space: 44.4 MB (68.12%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head=new ListNode();
+        int sum=l1.val+l2.val;
+        int value=sum%10;
+        int remain=sum/10;
+        head.val=value;
+         recursiveSol(l1.next,l2.next,head,remain);
+        return head;
+    }
+   private void recursiveSol(ListNode l1, ListNode l2, ListNode response, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return;
+        }
+
+        int sum = carry;
+
+        if (l1 != null) {
+            sum += l1.val;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+        }
+
+        int value = sum % 10;
+        int newCarry = sum / 10;
+
+        ListNode temp = new ListNode(value);
+        response.next = temp;
+
+        recursiveSol(l1 != null ? l1.next : null,
+                     l2 != null ? l2.next : null,
+                     response.next,
+                     newCarry);
+    }
+}

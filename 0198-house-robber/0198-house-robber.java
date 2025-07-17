7ece1dea6d0515b60b22604381ce85d49be299e5
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-07-18 00:08:50 +0530

    Time: 0 ms (100%), Space: 41.7 MB (8.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,10 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return recursiveSol(nums,0,0,dp);
+        // int[] dp=new int[nums.length+1];
+        // Arrays.fill(dp,-1);
+        // return recursiveSol(nums,0,0,dp);
+        return tabulation(nums);
     }
-
+    // Memoization
     int recursiveSol(int [] nums,int index,int max,int [] dp){
        if(index>=nums.length) return max;
        if(dp[index]!=-1) return dp[index];
@@ -13,4 +14,18 @@ class Solution {
        max+=Math.max(left,right);
        return dp[index]=max;
     }
+
+    //Tabulation
+    private int tabulation(int [] nums){
+        int[] dp=new int [nums.length];
+        if(nums.length==1) return nums[0];
+        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
 }

15e91a890cde3dc083363e6fe7b28a96678fa1e9
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-07-23 00:28:19 +0530

    Time: 0 ms (100%), Space: 40.4 MB (76.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,10 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return recursion(0,0,m,n,dp);
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+       // return recursion(0,0,m,n,dp);
+        tabulation(m,n,dp);
+        return dp[m-1][n-1];
     }
 
     private int recursion(int i,int j,int m,int n,int[][] dp){
@@ -13,5 +15,20 @@ class Solution {
         if(i==m-1 && j==n-1) return 1;
         if(dp[i][j] !=-1) return dp[i][j];
         return dp[i][j]=recursion(i,j+1,m,n,dp)+recursion(i+1,j,m,n,dp) ;
+
+    }
+
+    private void tabulation(int m,int n,int[][] dp){
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+         for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
     }
 }

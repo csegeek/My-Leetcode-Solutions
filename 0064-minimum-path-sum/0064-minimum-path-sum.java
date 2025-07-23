ae47bafdef02a58b6fd421652905809bacadcab1
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-07-24 01:59:19 +0530

    Time: 5 ms (27.58%), Space: 47.6 MB (68.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,10 +3,11 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-    return recursive(m-1,n-1,grid,dp);  
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+   // return recursive(m-1,n-1,grid,dp); 
+   return tabulation(grid,dp,m,n);
     }
 
     private int recursive(int i,int j,int[][] grid,int[][]dp){
@@ -19,6 +20,23 @@ class Solution {
         if(j>=1) up= recursive(i,j-1,grid,dp)+grid[i][j];
         if(i>=1) left=recursive(i-1,j,grid,dp)+grid[i][j];
         return dp[i][j]= Math.min(up,left);
+    }
+
+    private int tabulation(int[][]grid,int [][]dp,int m,int n){
+
+       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+            if(i==0 && j==0) dp[i][j]=grid[i][j];
+            else if(i==0) dp[i][j]=grid[i][j]+dp[i][j-1];
+            else if(j==0) dp [i][j]=grid[i][j]+dp[i-1][j];
+            else{
+               dp[i][j]= Math.min(grid[i][j]+dp[i-1][j],grid[i][j]+dp[i][j-1]);
+            }
+            }
+        }
+        return dp[m-1][n-1];
+
     }
 
 

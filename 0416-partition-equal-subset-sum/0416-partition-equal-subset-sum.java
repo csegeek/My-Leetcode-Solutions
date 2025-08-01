481fa7373c12fcd321c7aab5905607c5c345bae6
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-08-02 01:51:25 +0530

    Time: 52 ms (43.98%), Space: 47.6 MB (49.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,8 +6,9 @@ class Solution {
         }
         if(sum%2 !=0) return false;
         int target=sum/2;
-        Boolean[][] dp=new Boolean[target+1][nums.length];
-        return recursion(target,0,nums,dp);
+        //Boolean[][] dp=new Boolean[target+1][nums.length];
+        //return recursion(target,0,nums,dp);
+        return  tabulation(nums,target);
     }
     boolean recursion(int s,int i,int[] nums,Boolean[][] dp){
         if(s==0) return true;
@@ -20,4 +21,33 @@ class Solution {
         boolean right= recursion(s,i+1,nums,dp);
         return dp[s][i] =left|| right;
     }
+
+boolean tabulation(int[] nums, int target) {
+    int n = nums.length;
+    boolean[][] dp = new boolean[n][target + 1];
+
+    // Initialize base case: sum 0 can be achieved by taking nothing
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = true;
+    }
+
+    // Initialize for the first element
+    if (nums[0] <= target) {
+        dp[0][nums[0]] = true;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int sum = 1; sum <= target; sum++) {
+            boolean notTake = dp[i - 1][sum];
+            boolean take = false;
+            if (sum >= nums[i]) {
+                take = dp[i - 1][sum - nums[i]];
+            }
+            dp[i][sum] = take || notTake;
+        }
+    }
+
+    return dp[n - 1][target];
+}
+
 }

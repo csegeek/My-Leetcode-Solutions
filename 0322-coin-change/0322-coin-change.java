b0aefef8710c70f4dee23d05ec38c29c218dcbeb
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-08-18 01:10:18 +0530

    Time: 33 ms (16.63%), Space: 47.6 MB (5.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,9 +2,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         if(coins.length==0) return -1;
         if(coins.length==1 && amount%coins[0] !=0 ) return -1;
-        Integer[][] dp=new Integer[coins.length][amount+1];
-        int ans=recursion(coins.length-1,coins,amount,dp);
-
+       // Integer[][] dp=new Integer[coins.length][amount+1];
+        int ans=tabulation(coins,amount);
       return ans!=1e9 ? ans : -1;  
     }
 
@@ -25,5 +24,29 @@ class Solution {
       }
       int notPick=0+recursion(i-1,coins,amount,dp);
       return dp[i][amount]= Math.min(pick,notPick); 
+    }
+
+    int tabulation(int [] coins,int amount){
+      Integer[][] dp=new Integer[coins.length][amount+1];
+      for(int a=0;a<=amount;a++){
+        if(a%coins[0]==0){
+            dp[0][a]=a/coins[0];
+        }
+        else{
+            dp[0][a]=(int) 1e9;
+        }
+      }
+      for(int i=1;i<coins.length;i++){
+        for(int a=0;a<=amount;a++){
+            int pick=(int) +1e9;
+      if(coins[i]<=a){
+        pick=1+dp[i][a-coins[i]];
+      }
+      int notPick=0+dp[i-1][a];
+       dp[i][a]= Math.min(pick,notPick); 
+        }
+      }
+      return dp[coins.length-1][amount];
+
     }
 }

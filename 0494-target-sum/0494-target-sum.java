d175269eb33b79885afa7ee7eff610a495771deb
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-08-19 00:19:18 +0530

    Time: 353 ms (31.83%), Space: 41.1 MB (97.37%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,16 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return recursion(0,0,nums,target);
+        return recursion(0,nums.length-1,nums,target);
     }
 
     public int recursion(int sum,int i,int[] nums,int target){
-        if(i>=nums.length){
-        if(sum==target) return 1;
+        if(i==0){
+        if(sum+nums[i]==target && sum-nums[i]==target) return 2;
+        if(sum+nums[i]==target || sum-nums[i]==target) return 1;
         return 0;
         }
-       int sumw= recursion(sum+nums[i],i+1,nums,target);
-       int subt=recursion(sum-nums[i],i+1,nums,target);
+       int sumw= recursion(sum+nums[i],i-1,nums,target);
+       int subt=recursion(sum-nums[i],i-1,nums,target);
        return sumw+subt;
-
-
     }
 }

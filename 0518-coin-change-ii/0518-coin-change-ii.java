06179ab2da14c2de8335995e3a5bcbc876030905
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-08-21 01:47:40 +0530

    Time: 8 ms (68.81%), Space: 49.2 MB (42.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,10 +1,11 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp=new int[coins.length][amount+1];
-        for(int i=0;i<coins.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return recursion(coins.length-1,amount,coins,dp);
+        // int[][] dp=new int[coins.length][amount+1];
+        // for(int i=0;i<coins.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return recursion(coins.length-1,amount,coins,dp);
+        return tabulation(amount,coins);
     }
     int recursion(int i,int t,int [] coins,int[][] dp){
         if(t==0) return 1;
@@ -19,5 +20,27 @@ class Solution {
         }
         int notPick=recursion(i-1,t,coins,dp);
         return dp[i][t]= pick+notPick;
+    }
+    int tabulation(int amount,int[] coins){
+     int[][] dp=new int[coins.length][amount+1];
+     for(int i=0;i<coins.length;i++){
+        dp[i][0]=1;
+     }
+     for(int i=0;i<=amount;i++){
+        if(i%coins[0]==0)
+        dp[0][i]=1;
+     }
+     for(int i=1;i<coins.length;i++){
+        for(int t=0;t<=amount;t++){
+            int pick=0;
+        if(t>=coins[i]){
+            pick=dp[i][t-coins[i]];
+        }
+        int notPick=dp[i-1][t];
+         dp[i][t]= pick+notPick;
+        }
+     }
+   return dp[coins.length-1][amount];
+
     }
 }

882152a2dd1f4f38626558695918e388d0a6ff9c
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-02 00:40:34 +0530

    Time: 20 ms (39.73%), Space: 51 MB (79.41%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,20 +1,41 @@
 class Solution {
     public int numDistinct(String s, String t) {
-     int[][] dp=new int[s.length()][t.length()];
-     for(int[] dpi:dp){
-        Arrays.fill(dpi,-1);
-     }
-     return recursion(s.length()-1,t.length()-1,s,t,dp);   
+        // int[][] dp = new int[s.length() + 1][t.length() + 1];
+        // for (int[] dpi : dp) {
+        //     Arrays.fill(dpi, -1);
+        // }
+        // return recursion(s.length(), t.length(), s, t, dp);
+        return tabulation(s,t);
     }
-    int recursion(int i,int j,String s,String t,int[][]dp){
-      if(j<0) return 1;
-      if(i<0) return 0;
-      if(dp[i][j]!=-1) return dp[i][j];
-      if(s.charAt(i)==t.charAt(j)){
-        return dp[i][j]=recursion(i-1,j-1,s,t,dp)+recursion(i-1,j,s,t,dp);
-      }
-      else{
-        return dp[i][j]=recursion(i-1,j,s,t,dp);
-      }
+
+    int recursion(int i, int j, String s, String t, int[][] dp) {
+        if (j <= 0)
+            return 1;
+        if (i <= 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
+            return dp[i][j] = recursion(i - 1, j - 1, s, t, dp) + recursion(i - 1, j, s, t, dp);
+        } else {
+            return dp[i][j] = recursion(i - 1, j, s, t, dp);
+        }
+    }
+
+    int tabulation(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 }

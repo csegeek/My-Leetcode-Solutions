77cf2768edef96fcd0d6e191bd9a19e215fa7338
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-03 01:09:26 +0530

    Time: 2 ms (100%), Space: 45.1 MB (46.22%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp=new int[word1.length()][word2.length()];
+        for(int[] dpi:dp){
+            Arrays.fill(dpi,-1);
+        }
+       return recursion(word1.length()-1,word2.length()-1,word1,word2,dp);
+    }
+
+    int recursion(int i,int j,String word1,String word2,int[][]dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j]=recursion(i-1,j-1,word1,word2,dp);
+        }
+        else{
+            int insert=1+recursion(i,j-1,word1,word2,dp);
+            int delete=1+recursion(i-1,j,word1,word2,dp);
+            int replace=1+recursion(i-1,j-1,word1,word2,dp);
+            return dp[i][j]=(Math.min(insert,Math.min(delete,replace)));
+        }
+    }
+}

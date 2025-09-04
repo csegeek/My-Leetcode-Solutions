5e00a4a5915895e6c6f0cab4111bf1059492facb
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-05 00:22:29 +0530

    Time: 1 ms (77.48%), Space: 46.2 MB (27.38%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,39 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp=new int[prices.length][2];
-        for(int[] dpi:dp){
-            Arrays.fill(dpi,-1);
-        }
-     return recursion(0,1,prices,dp);  
+        // int[][] dp = new int[prices.length][2];
+        // for (int[] dpi : dp) {
+        //     Arrays.fill(dpi, -1);
+        // }
+        // return recursion(0, 1, prices, dp);
+        return tabulation(prices);
     }
-    int recursion(int i,int buy,int[] prices,int[][] dp){
-        if(i==prices.length) return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy==1){
-           return dp[i][buy]= Math.max(-prices[i]+recursion(i+1,0,prices,dp),recursion(i+1,1,prices,dp));
+
+    int recursion(int i, int buy, int[] prices, int[][] dp) {
+        if (i == prices.length)
+            return 0;
+        if (dp[i][buy] != -1)
+            return dp[i][buy];
+        if (buy == 1) {
+            return dp[i][buy] = Math.max(-prices[i] + recursion(i + 1, 0, prices, dp), recursion(i + 1, 1, prices, dp));
+        } else {
+            return dp[i][buy] = Math.max(prices[i] + recursion(i + 1, 1, prices, dp), recursion(i + 1, 0, prices, dp));
+        }
+    }
+
+    int tabulation(int [] prices){
+        int[][] dp=new int[prices.length+1][2];
+        for(int i=prices.length-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+              if(buy==1){
+            dp[i][buy]= Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
         }
         else{
-           return dp[i][buy]=Math.max(prices[i]+recursion(i+1,1,prices,dp),recursion(i+1,0,prices,dp));
+            dp[i][buy]=Math.max(prices[i]+dp[i+1][1],dp[i+1][0]);
+        }   
+            }
         }
+
+        return dp[0][1];
+        
     }
 }

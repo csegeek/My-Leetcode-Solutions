a1d0e564cdd4d7665189c48450ce4431e6de8256
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-05 23:33:06 +0530

    Time: 175 ms (6.17%), Space: 81.1 MB (13.86%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int [][][] dp=new int [prices.length][2][3];
+        for(int[][] dpi:dp){
+            for(int[] dpj:dpi){
+                Arrays.fill(dpj,-1);
+            }
+        }
+       return recursion(0,1,2,prices,dp); 
+    }
+    int recursion(int i,int buy,int cap,int[] prices,int [][][] dp){
+        if(cap==0) return 0;
+        if(i>=prices.length) return 0;
+        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
+        if(buy==1){
+            return dp[i][buy][cap]=Math.max(-prices[i]+recursion(i+1,0,cap,prices,dp),recursion(i+1,1,cap,prices,dp));
+        }
+        else{
+            return dp[i][buy][cap]=Math.max(prices[i]+recursion(i+1,1,cap-1,prices,dp),recursion(i+1,0,cap,prices,dp));
+        }
+    }
+}

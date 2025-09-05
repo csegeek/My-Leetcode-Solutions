97d902be1feaa508c16d725aa17f551f9e20368e
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-06 00:37:47 +0530

    Time: 2 ms (83.56%), Space: 45 MB (35.12%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int [][][] dp=new int [prices.length][2][k+1];
-        for(int[][] dpi:dp){
-            for(int[] dpj:dpi){
-                Arrays.fill(dpj,-1);
-            }
-        }
-     return recursion(0,1,k,prices,dp);
+    //     int [][][] dp=new int [prices.length][2][k+1];
+    //     for(int[][] dpi:dp){
+    //         for(int[] dpj:dpi){
+    //             Arrays.fill(dpj,-1);
+    //         }
+    //     }
+    //  return recursion(0,1,k,prices,dp);
+    return tabulation(prices,k);
     }
 
     int recursion(int i, int buy, int cap, int[] prices, int[][][] dp) {
@@ -24,5 +25,20 @@ class Solution {
                     recursion(i + 1, 0, cap, prices, dp));
         }
     }
-
+    int tabulation(int [] prices,int k){
+     int [][][] dp=new int [prices.length+1][2][k+1];
+     for(int i=prices.length-1;i>=0;i--){
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=1;cap<=k;cap++){
+               if(buy==1){
+             dp[i][buy][cap]=Math.max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+        }
+        else{
+            dp[i][buy][cap]=Math.max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);   
+            }
+        }
+     }    
+    }
+    return dp[0][1][k];
+}
 }

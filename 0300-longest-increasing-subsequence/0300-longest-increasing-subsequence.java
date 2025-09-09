2107fc1bd9bf81156266f624a0a3243e9807fcd6
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-10 00:07:14 +0530

    Time: 44 ms (47.29%), Space: 44.5 MB (50.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,10 +1,11 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int [][] dp=new int [nums.length][nums.length+1];
-        for(int[] dpi:dp){
-            Arrays.fill(dpi,-1);
-        }
-      return recursion(0,-1,nums,dp);  
+    //     int [][] dp=new int [nums.length][nums.length+1];
+    //     for(int[] dpi:dp){
+    //         Arrays.fill(dpi,-1);
+    //     }
+    //   return recursion(0,-1,nums,dp);  
+    return tabulation(nums);
     }
 
     int recursion(int i,int prev,int[] nums,int[][] dp){
@@ -16,5 +17,20 @@ class Solution {
      }
         int notPick=recursion(i+1,prev,nums,dp);
         return dp[i][prev+1]=Math.max(pick,notPick);
+     }
+
+     int tabulation(int [] nums){
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,1);
+        int max=1;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<=i-1;j++){
+                if(nums[i]>nums[j]){
+                dp[i]=Math.max(dp[i],1+dp[j]);
+                } 
+            }
+            max=Math.max(dp[i],max);
+        }
+        return max;
      }
 }

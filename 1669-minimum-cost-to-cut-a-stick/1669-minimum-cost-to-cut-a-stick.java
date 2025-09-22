337490628865d0e851fd106ac80139d8f5deafc1
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-23 00:43:43 +0530

    Time: 23 ms (46.89%), Space: 42.9 MB (27.61%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -12,6 +12,7 @@ class Solution {
         arr[cuts.length+1]=n;
         Arrays.sort(arr);
       return recursion(1,arr.length-2,arr,dp);
+  //  return tabulation(n,cuts);
     }
 
     int recursion(int i, int j,int[] arr,int[][] dp) {
@@ -23,5 +24,31 @@ class Solution {
                 min=Math.min(min,cost);
          }
          return dp[i][j]= min;
+    }
+
+    int tabulation(int n,int[] cuts){
+      int [][] dp=new int [cuts.length+2][cuts.length+2];
+        for(int [] dpi:dp){
+            Arrays.fill(dpi,-1);
+        }
+        int [] arr=new int[cuts.length+2];
+        arr[0]=0;
+        for(int i=0;i<cuts.length;i++){
+            arr[i+1]=cuts[i];
+        }
+        arr[cuts.length+1]=n;
+        Arrays.sort(arr);
+        for(int i=cuts.length-1;i>0;i--){
+            for(int j=1;j<cuts.length;j++){
+                if(i>j) continue;
+                int min =(int) 1e9;
+         for(int k=i;k<=j;k++){
+                int cost=arr[j+1]-arr[i-1]+dp[i][k-1]+dp[k+1][j];
+                min=Math.min(min,cost);
+         }
+          dp[i][j]= min; 
+            }
+        } 
+        return dp[1] [arr.length-2];
     }
 }

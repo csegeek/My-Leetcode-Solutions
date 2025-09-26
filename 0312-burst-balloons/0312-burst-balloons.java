9f7034f936f1ce468e67b2fe20e2e6b262a67ef8
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-09-27 00:25:02 +0530

    Time: 56 ms (62.65%), Space: 45.4 MB (5.43%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        int[] numsTemp=new int[nums.length+2];
-        int[][] dp=new int [nums.length+1][nums.length+1];
-        for(int[] dpi:dp){
-            Arrays.fill(dpi,-1);
-        }
-        numsTemp[0]=1;
-        for(int i=1;i<=nums.length;i++){
-            numsTemp[i]=nums[i-1];
-        }
-        numsTemp[nums.length+1]=1;
-     return recursion(1,nums.length,numsTemp,dp);  
+    //     int[] numsTemp=new int[nums.length+2];
+    //     int[][] dp=new int [nums.length+1][nums.length+1];
+    //     for(int[] dpi:dp){
+    //         Arrays.fill(dpi,-1);
+    //     }
+    //     numsTemp[0]=1;
+    //     for(int i=1;i<=nums.length;i++){
+    //         numsTemp[i]=nums[i-1];
+    //     }
+    //     numsTemp[nums.length+1]=1;
+    //  return recursion(1,nums.length,numsTemp,dp);  
+    return tabulation(nums);
     }
 
 
@@ -24,6 +25,32 @@ class Solution {
         }
         return  dp[i][j]=max;
     }
+
+int tabulation(int [] nums){
+     int[] numsTemp=new int[nums.length+2];
+     int[][] dp=new int [nums.length+2][nums.length+2];
+        numsTemp[0]=1;
+        for(int i=1;i<=nums.length;i++){
+            numsTemp[i]=nums[i-1];
+        }
+        numsTemp[nums.length+1]=1;
+    for(int i=nums.length;i>0;i--){
+        for(int j=0;j<=nums.length;j++){
+         if(i>j) continue;
+        int max=(int)-1e9;
+        for(int k=i;k<=j;k++){
+            int coins=numsTemp[i-1]*numsTemp[k]*numsTemp[j+1]+dp[i][k-1]+dp[k+1][j];
+            max=Math.max(max,coins);
+        }
+        dp[i][j]=max;
+        }
+    }
+    for(int [] dpi:dp){
+        System.out.println(Arrays.toString(dpi));
+    }
+ return dp[1][nums.length];
+}
+    
 
 
     

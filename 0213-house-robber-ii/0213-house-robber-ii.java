cbd1e8217e25aae64828e079686850ab94096dad
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2025-11-17 23:17:38 +0530

    Time: 0 ms (100%), Space: 42.4 MB (24.85%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int [] temp1=new int[nums.length-1];
-        int [] temp2=new int [nums.length-1];
-        for(int i=0;i<nums.length;i++){
-            if(i!=0) temp1[i-1]=nums[i];
-            if(i!=nums.length-1) temp2[i]=nums[i];
-        }
-        return Math.max(tabulation(temp1),tabulation(temp2));
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int[] dp1=new int [nums.length];
+        int[] dp2=new int [nums.length];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        return Math.max(recursion(0,n-2,nums,dp1),recursion(1,n-1,nums,dp2));
     }
 
-     private int tabulation(int [] nums){
-        int[] dp=new int [nums.length];
-        if(nums.length==1) return nums[0];
-        if(nums.length==2) return Math.max(nums[0],nums[1]);
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[nums.length-1];
+    int recursion(int i,int end, int[] nums,int [] dp) {
+    if (i > end) return 0;
+    if(dp[i]!=-1) return dp[i];
+    int rob = nums[i] + recursion(i + 2,end, nums,dp);
+    int skip = recursion(i + 1,end, nums,dp);
+    return dp[i]=Math.max(rob, skip);
     }
 }

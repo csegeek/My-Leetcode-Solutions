577d7d2abda01a40749cab97a5c12e75cf120f88
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2026-02-15 13:42:45 +0530

    Time: 356 ms (6.19%), Space: 48.1 MB (41.3%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        Boolean[] dp=new Boolean [nums.length];
+        return recursive(nums,0,dp);
+    }
+    private boolean recursive(int[] nums,int ind,Boolean[] dp){
+        if(ind>=nums.length-1) return true;
+        if(dp[ind]!=null) return dp[ind];
+        for(int stp=1;stp<=nums[ind];stp++){
+            if(recursive(nums,ind+stp,dp)) return dp[ind]=true;
+        }
+        return dp[ind]=false;
+    }
+}
